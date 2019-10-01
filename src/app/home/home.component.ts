42c213c92e57d8d54c3e604bf89172703a0b72b6
atanas.bochukov <atanas.bochukov@bulpros.com>
2019-10-01 09:58:35 +0300

    Finish gallery in details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -91,23 +91,23 @@ export class HomeComponent implements OnInit {
     setTimeout(() => {
       const current = document.querySelector('#selected');
       const thumbs = document.querySelectorAll('.thumbs img');
-      const opacity = 0.5;
+      const opacity = '0.5';
 
-      // thumbs[0].style.opacity = opacity;
+      (thumbs[0] as HTMLElement).style.opacity = opacity;
 
       
       thumbs.forEach(img => img.addEventListener('click', imgActivate));
 
       function imgActivate(e) {
-        // thumbs.forEach(img => (img.style.opacity = 1));
+        thumbs.forEach(img => ((img as HTMLElement).style.opacity = '1'));
 
-        current.src = e.target.src;
+        (current as HTMLImageElement).src = e.target.src;
 
         current.classList.add('fade-in');
 
         setTimeout(() => current.classList.remove('fade-in'), 500);
 
-        // e.target.style.opacity = opacity;
+        e.target.style.opacity = opacity;
       }
 
 
