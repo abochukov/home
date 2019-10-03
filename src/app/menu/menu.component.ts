1576946d19d1291fb3fac023cdf2bd36d61744f5
atanas.bochukov <atanas.bochukov@bulpros.com>
2019-10-03 15:52:51 +0300

    Move search code in menu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, TemplateRef, ViewChild, ElementRef } from '@angular/core';
 
 import { ToggleCategoriesService } from '../toggle-categories.service';
+import { DataService } from '../data.service';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
 
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
@@ -9,10 +14,13 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('searchInput', {static: false}) searchInput: ElementRef;
   modalRef: BsModalRef;
   public buttonClicked: boolean = false;
+  public searchingString;
+  public searchResults;
 
   config = {
     animated: false,
@@ -22,9 +30,17 @@ export class MenuComponent implements OnInit {
     class: "modal-lg",
   };
 
-  constructor(private modalService: BsModalService, private toggleCategoriesService: ToggleCategoriesService) { }
+  constructor(
+    private modalService: BsModalService, 
+    private toggleCategoriesService: ToggleCategoriesService,
+    private dataService: DataService,
+    ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.search()
   }
 
   openModal(template: TemplateRef<any>) {
@@ -34,5 +50,15 @@ export class MenuComponent implements OnInit {
   public showCategoriesOnMobile() {
     this.buttonClicked = !this.buttonClicked;
     this.toggleCategoriesService.sendStatus(this.buttonClicked);
+  }
+
+  search() {
+    this.searchingString = Observable.fromEvent(this.searchInput.nativeElement, 'keyup').debounceTime(400).subscribe((value) => {
+      console.log(this.searchInput.nativeElement.value)
+      this.dataService.search(this.searchInput.nativeElement.value).subscribe(data => {
+        this.searchResults = data;
+        console.log(this.searchResults);
+      });
+    });
   }
 }
