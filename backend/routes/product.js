1358444cdf85d12cae4a27d34438db561c4a4856
atanas.bochukov <atanas.bochukov@bulpros.com>
2019-11-14 17:42:50 +0200

    Create new route for selecting product by product id

---
+++ backend/routes/product.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var sql = require('../data/config');
+// var bodyParser = require('body-parser');
+
+
+router.get('/:id', (req, res) => {
+    sql.query("SELECT * FROM test.products WHERE id = '"+ req.params.id +"'", (error, result, fields) => {
+        if (!!error) {
+            console.log('error in the query');
+            res.send(error);
+        } else {  
+            console.log('successful query');
+            console.log(result);
+            res.send(result);
+        }
+    })
+});
+
+module.exports = router;
