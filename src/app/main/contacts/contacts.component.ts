e6462d4ae7fceeddd38a148cb9a9c8644aaa127a
atanas.bochukov <atanas.bochukov@bulpros.com>
2020-01-24 11:05:54 +0200

    Rename myForm to contactForm

--- src/app/main/contacts/contacts.component.ts
+++ src/app/main/contacts/contacts.component.ts
@@ -10,48 +10,48 @@ import { DataService } from '../../data.service';
 })
 export class ContactsComponent implements OnInit {
 
-  public myForm: FormGroup;
+  public contactForm: FormGroup;
   public formattedMessage: string;
   public errorMessage: string;
 
   constructor(private fb: FormBuilder, private dataService: DataService) { }
 
   ngOnInit() {
-    this.myForm = this.fb.group({
+    this.contactForm = this.fb.group({
       name:  ['', [Validators.required, Validators.minLength(3)]],
       phone: ['', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(8), Validators.maxLength(12)]],
       email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
       message: ['', Validators.required]
     });
 
-    this.myForm.get('name').statusChanges.subscribe(name => {
+    this.contactForm.get('name').statusChanges.subscribe(name => {
       console.log(name);
     })
 
   }
 
   onSubmit() {
-    if(this.myForm.valid) {
-      console.log(this.myForm.value.name);
-      this.dataService.sendMail(this.myForm.value).subscribe(data => {
+    if(this.contactForm.valid) {
+      console.log(this.contactForm.value.name);
+      this.dataService.sendMail(this.contactForm.value).subscribe(data => {
       }, error => {
         if(error.statusText == 'OK') {
-          this.myForm.reset();
+          this.contactForm.reset();
         }
       })
     } else {
-      if(this.myForm.controls.name.invalid) {
+      if(this.contactForm.controls.name.invalid) {
         this.errorMessage = 'Моля попълнете име'
-      } else if(this.myForm.controls.phone.invalid) {
+      } else if(this.contactForm.controls.phone.invalid) {
         this.errorMessage = 'Полето телефонен номер трявба да съдържа само цифри и валиден номер'
-      } else if(this.myForm.controls.email.invalid) {
+      } else if(this.contactForm.controls.email.invalid) {
         this.errorMessage = 'Моля въведете валиден e-mail адрес'
       }
     }
   }
 
   onReset() {
-    this.myForm.reset();
+    this.contactForm.reset();
   }
 
 }
