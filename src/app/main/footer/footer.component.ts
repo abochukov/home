1825567c293aae3d8eb35697cb4d1c923948d688
atanas.bochukov <atanas.bochukov@bulpros.com>
2020-03-17 07:14:50 +0200

    Add terms in footer

--- src/app/main/footer/footer.component.ts
+++ src/app/main/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, TemplateRef, ChangeDetectorRef } from '@angular/core
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 import { ShowCartItemsService } from '../../common/services/show-cart-items.service';
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-footer',
@@ -13,6 +14,7 @@ export class FooterComponent implements OnInit {
 
   public cartItems: any;
   modalRef: BsModalRef;
+  public terms: any;
 
   config = {
     animated: false,
@@ -25,10 +27,14 @@ export class FooterComponent implements OnInit {
   constructor(
     private modalService: BsModalService, 
     private showCartItemsService: ShowCartItemsService,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    private dataService: DataService,
   ) { }
 
   ngOnInit() {
+    this.dataService.getTerms().subscribe(data => {
+      this.terms = data[0].content;
+    });
   }
 
   public showConditions(template: TemplateRef<any>) {
