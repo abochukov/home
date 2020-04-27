0b804a9dbf92b776fb210373c1342415b7a3f737
atanas.bochukov <atanas.bochukov@bulpros.com>
2020-04-27 21:55:48 +0300

    If there's no results show alert

--- src/app/main/products/home/home.component.ts
+++ src/app/main/products/home/home.component.ts
@@ -126,8 +126,8 @@ export class HomeComponent implements OnInit, OnChanges, AfterViewInit {
 
     // console.log(window.location.href)
 
-    // if(window.location.href == 'http://localhost:4200/home') {
-    if(window.location.href == 'https://profitstore.bg/home') {
+    if(window.location.href == 'http://localhost:4200/home') {
+    // if(window.location.href == 'https://profitstore.bg/home') {
       category = 1;
       area = 7;
     } else {
@@ -252,7 +252,11 @@ export class HomeComponent implements OnInit, OnChanges, AfterViewInit {
     let searchString = this.searchInput.nativeElement.value;
     if(searchString) {
       this.dataService.search(searchString).subscribe(data => {
-        this.products = data;
+        if(data == null) {
+          alert('Не са намерени резултати съответстващи на вашите критерии')
+        } else {
+          this.products = data;
+        }
         this.updatePagination();
       });
     }
